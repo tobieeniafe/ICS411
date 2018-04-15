ccd4c6c84dd0249d52b7798be65708b7a048d35d
Ali Yisa <aliyisa@outlook.com>
2018-04-15 22:20:22 +0100

    change db host

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(0);
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "haematology";
+$dbhost = "db4free.net";
+$dbuser = "aligorithm";
+$dbpass = "123456";
+$dbname = "haematoloji";
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 if (mysqli_connect_errno()) {
 	echo "error to connect to database : ".mysqli_connect_errno();
