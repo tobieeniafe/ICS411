da5ca0d3e86829ab890e3a197eb3b0cf0dd23055
Ali Yisa <aliyisa@outlook.com>
2018-04-15 23:54:15 +0100

    backback

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,12 @@
 <?php
-error_reporting(0);
-$dbhost = "db4free.net";
-$dbuser = "aligorithm";
-$dbpass = "123456";
-$dbname = "haematoloji";
+// error_reporting(0);
+$dbhost = "127.0.0.1";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "haematology";
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 if (mysqli_connect_errno()) {
-	echo "error to connect to database : ".mysqli_connect_errno();
+	die("error to connect to database : ".mysqli_connect_errno());
 }
 else {
 	//echo "connection sucessful";
